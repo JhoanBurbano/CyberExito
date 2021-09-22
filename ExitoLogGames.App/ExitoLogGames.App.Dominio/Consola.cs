495dbd18a4cb0dad808f47f1981be8a872236bc9
ValenArias08 <valentina.ariasv20@gmail.com>
2021-09-21 21:40:25 -0500

    add Consola.cs/Dominio

---
+++ ExitoLogGames.App/ExitoLogGames.App.Dominio/Consola.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExitoLogGames.App.Dominio
+{
+    public class Consola
+    {
+        public string CapacidadAlmacenamiento {get; set;};
+        public string VelocidadRam {get; set;}
+        public string VelocidadProcesamiento {get; set;}
+        public int CantidadControles {get; set;}
+        public string Storage {get; set;}
+    }
+}
