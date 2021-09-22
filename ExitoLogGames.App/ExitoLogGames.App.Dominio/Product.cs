32c4c4bef4edf1f6c25922cc7416a2ff2b931a30
Jhoan Burbano <jhoanburbano@unicauca.edu.co>
2021-09-21 21:21:29 -0500

    create class Product

--- ExitoLogGames.App/ExitoLogGames.App.Dominio/Product.cs
+++ ExitoLogGames.App/ExitoLogGames.App.Dominio/Product.cs
@@ -3,6 +3,11 @@ namespace ExitoLogGames.App.Dominio
     public class Product
     {
         public int IdProduct { get; set;}
+        public string Nombre { get; set;}
+        public string Version { get; set;}
+        public string Fabricante { get; set;}
+        public double Pecio { get; set;}
+        public double Costo { get; set;}
 
     }
 }
