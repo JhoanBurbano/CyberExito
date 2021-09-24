056d8f3cceeb07db15d8fa94b65a9162632e19dc
ValenArias08 <valentina.ariasv20@gmail.com>
2021-09-24 13:12:24 -0500

    add RepositorioConsolas.cs/Comandos

--- ExitoLogGames.App/ExitoLogGames.App.Comandos/RepositorioConsolas.cs
+++ ExitoLogGames.App/ExitoLogGames.App.Comandos/RepositorioConsolas.cs
@@ -1,23 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ExitoLogGames.App.Persistencia;
 using ExitoLogGames.App.Dominio;
+using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace ExitoLogGames.App.Comandos
 {
     public class RepositorioConsolas : IRepositorioConsolas
     {
-        private readonly Connection conexion;  
-
+        private readonly Connection conexion;
         public RepositorioConsolas(Connection conexion){
             this.conexion = conexion;
         }
 
-        IEnumerable<Consola> IrepositorioConsolas.GetAllConsolas(){
+        IEnumerable<Consolas> IrepositorioConsolas.GetAllConsolas(){
             return conexion.consolas;
-        };
+        }
 
         Consola IrepositorioConsolas.AddConsola(Consola consola){
             var consol = conexion.consolas.Add(consola);
@@ -26,7 +24,7 @@ namespace ExitoLogGames.App.Comandos
         }
 
         Consola IrepositorioConsolas.UpdateConsola(Consola consola){
-            var consol = conexion.consolas.FindOrDefault( c => c.id = c.Cedula);
+            var consol = conexion.consolas.FirstOrDefault( c => c.id = c.Cedula);
             if(consol!=null){
                 consol.CapacidadAlmacenamiento=consola.CapacidadAlmacenamiento;
                 consol.VelocidadRam=consola.VelocidadRam;
@@ -38,17 +36,15 @@ namespace ExitoLogGames.App.Comandos
         }
 
         void IrepositorioConsolas.DeleteConsola(int IdConsola){
-            var consol = conexion.Consola.FindOrDefault( c => v.id == IdConsola);
-            if(consol!=null){
-                conexion.consolas.remove (vend);
-                Console.WriteLines("Se elimino la consola correctamente");
-            }
-            return consol;
-            
+            var consol = conexion.consolas.FirstOrDefault( c => c.id == IdConsola);
+            if(consol!=null)
+                return;
+            conexion.consolas.Remove(consol);
+            Console.WriteLines("Se elimino la consola correctamente");            
         }
 
         Consola IrepositorioConsolas.GetConsola(int IdConsola){
-            return conexion.consolas.FindOrDefault( c => c.Id == IdConsola);
+            return conexion.consolas.FirstOrDefault( c => c.Id == IdConsola);
         }
 
     }
